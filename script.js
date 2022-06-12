5e19e54d01ca47b4492d0707eeacc15c4e47e1eb
JayAlwaysCodes <johnsonjardocs@gmail.com>
2022-06-12 02:59:17 +0100

    worked a little on the responsiveness

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function showForm(){
         form.style.display='none'
     }
 }
+
+function show_menu(){
+    var menu = document.getElementById('services')
+    if(menu.style.display==='none'){
+        menu.style.display='block'
+    }else{
+        menu.style.display='none'
+    }
+}
